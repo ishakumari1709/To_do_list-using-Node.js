0ed31429ba50194a00c7bf6b2ff1d0d3c4661982
Isha Kumari <144309840+ishakumari1709@users.noreply.github.com>
2026-02-06 10:11:33 +0530

    Create Task.js

---
+++ models/Task.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ["pending", "done"],
+      default: "pending"
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
